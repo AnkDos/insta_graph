eca4d88ccec743a51f1e0eab636c78d700adb1c3
ankdos <ankurpan96@gmail.com>
2019-06-02 00:14:21 +0530

    added necessary function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
-function get_access_token(){
+
+function get_access_token(){ 
+
+     load_cookie = document.cookie;
+     
+     if(load_cookie == null){
+          url =  window.location.href ;
+          if(url.includes('#')){
+          split = url.split('#');
+          more_split = split[1].split('=') ;
+          access_token = more_split[1] ;   
+          document.cookie = '{"at":${access_token} }';
+          console.log(access_token);
+          return access_token ;
+     }else{
+          get_token = JSON.parse(load_cookie);
+          console.log("reached");
+          console.log(get_token.at);
+          return get_token.at;
+          }  
+     }
    
-    url =  window.location.href ;
-    if(url.includes('#')){
-        split = url.split('#');
-        more_split = split[1].split('=') ;
-        access_token = more_split[1] ;   
-        console.log(access_token) ;
-   }else{
-        console.log('Not Here') ;
-   }
     
 }
