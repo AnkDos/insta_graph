ab1df0c60285441100fd4977220e79e8b9e3a488
ankdos <ankurpan96@gmail.com>
2019-06-03 16:53:57 +0530

    basic design done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 function get_access_token(){ 
 
      load_cookie = document.cookie;
@@ -12,14 +16,60 @@ function get_access_token(){
           document.cookie = JSON.stringify(cookie_json);
           console.log(access_token);
           return access_token ;
+      }else{
+           return null ;           
       }
      }
-     else{
+     else if(load_cookie.indexOf(':') != -1){
           get_token = JSON.parse(load_cookie);
           console.log("reached");
           console.log(get_token.at);
           return get_token.at;
+          }
+     else{
+          return null ;
           }  
-     }
-   
+     
+}
+
+function create_graph(){
     
+      access_token = get_access_token();
+     
+
+ 
+          new Chart(document.getElementById("canvas"), {
+               type: 'line',
+               data: {
+                 labels: ['filter1','filter2' , 'filter3','filter4'],
+                 datasets: [{ 
+                     data: [100,120,110,125],
+                     label: "Africa",
+                     borderColor: "#3e95cd",
+                     fill: false
+                   }
+                 ]
+               },
+               options: {
+                  responsive: true,
+                 title: {
+
+                   display: true,
+                   text: 'World population per region (in millions)'
+                 }
+               }
+             });
+
+
+      }
+
+
+
+
+
+
+
+
+
+
+
